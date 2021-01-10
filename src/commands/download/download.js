350719e77c7139c497af5b0d2e525a0a38e01770
Maxime Charron <mcharron@naoned.fr>
2021-01-10 19:19:26 +0100

    feat: discord bot is working

---
+++ src/commands/download/download.js
@@ -0,0 +1,83 @@
+require('dotenv').config()
+
+const { Command } = require('discord.js-commando');
+
+const fetch = require('node-fetch')
+const https = require('https')
+const fs = require('fs')
+
+const apiKey = process.env.API_KEY
+const tokenApiUrl = "https://api.1fichier.com/v1/download/get_token.cgi"
+const fileinfoApiUrl = "https://api.1fichier.com/v1/file/info.cgi"
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer ' + apiKey
+}
+
+module.exports = class DownloadCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'download',
+      aliases: ['dl'],
+      group: 'datamanagement',
+      memberName: 'download',
+      description: 'Launch file download',
+      clientPermissions: ['SEND_MESSAGES']
+    })
+  }
+  
+  async run(msg) {
+    let postData = {
+      'url': msg.content.replace('+dl ', '')
+    }
+    const fileInfo = await fetch(fileinfoApiUrl, {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: headers
+    })
+    const file = await fileInfo.json()
+    const response = await fetch(tokenApiUrl, {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: headers
+    })
+    const json = await response.json()
+    if(json.status === "KO")
+    {
+      console.log('Erreur durant le fetch API (url: ' + postData['url'] + ')')
+      msg.say('Erreur durant le fetch API : ' + json.message)
+    }
+    else
+    {
+      console.log("Start downloading " + file.filename + "...")
+      mqg.say('Téléchargement de ' + file.filename + ' en cours')
+      const output = fs.createWriteStream("data/"+file.filename)
+      let responseData = ''
+      let lastPercent = 0
+      const download = https.get(json.url, (response) => {
+        responseLength = response.headers['content-length']
+        length = []
+
+        response.on('data', (d) => {
+          responseData += d
+          length.push(d.length)
+          let sum = length.reduce((a, b) => a + b, 0);
+          let completedPercentage = Math.floor((sum / responseLength) * 100)
+          if(completedPercentage > lastPercent) {
+            console.log(`${completedPercentage} % downloaded`)
+            lastPercent = completedPercentage
+          }
+          if(completedPercentage === 50)
+          {
+            msg.say('Téléchargement de ' + file.filename + ' à 50%')
+          }
+        })
+        response.on('end', () => {
+          output.write(responseData)
+          msg.say('Téléchargement de ' + file.filename + ' terminé')
+	})
+      })
+    }  
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,60 +1,25 @@
 require('dotenv').config()
 
-const fetch = require('node-fetch')
-const https = require('https')
-const fs = require('fs')
+const path = require('path')
 
-const apiKey = process.env.API_KEY
-const tokenApiUrl = "https://api.1fichier.com/v1/download/get_token.cgi"
-const fileinfoApiUrl = "https://api.1fichier.com/v1/file/info.cgi"
+const { CommandoClient } = require('discord.js-commando')
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Authorization': 'Bearer ' + apiKey
-}
+const client = new CommandoClient({
+  commandPrefix: '+',
+  owner: '206863244793479178'
+})
 
-let postData = {
-  'url': "https://1fichier.com/?eig8l6wbpnx27kqd91dm"
-};
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    [ 'datamanagement', 'Data Management' ]
+  ])
+  .registerCommandsIn(path.join(__dirname, 'commands'))
 
-(async() => {
-  const fileInfo = await fetch(fileinfoApiUrl, {
-    method: 'POST',
-    body: JSON.stringify(postData),
-    headers: headers
-  })
-  const file = await fileInfo.json()
-  const response = await fetch(tokenApiUrl, {
-    method: 'POST',
-    body: JSON.stringify(postData),
-    headers: headers
-  })
-  const json = await response.json()
-  if(json.status === "KO")
-  {
-    console.log('Erreur durant le fetch API');
-  }
-  else
-  {
-    console.log("Start downloading " + file.filename + "...")
-    const output = fs.createWriteStream("data/"+file.filename)
-    let responseData = ''
-    let lastPercent = 0
-    const download = https.get(json.url, (response) => {
-      responseLength = response.headers['content-length']
-      length = []
+client.once('ready', () => {
+  console.log('Bot ready')
+})
 
-      response.on('data', (d) => {
-        responseData += d
-        length.push(d.length)
-        let sum = length.reduce((a, b) => a + b, 0);
-        let completedPercentage = Math.floor((sum / responseLength) * 100)
-        if(completedPercentage > lastPercent) {
-	  console.log(`${completedPercentage} % downloaded`)
-          lastPercent = completedPercentage
-	}
-      })
-      response.on('end', () => output.write(responseData))
-    })
-  }
-})();
+client.on('error', console.error)
+
+client.login(process.env.DISCORD_KEY)
